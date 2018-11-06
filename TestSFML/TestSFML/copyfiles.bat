94cec88c574bdad290b58f28599cba6fe90feabf
Ivan <hector_2003_20@hotmail.com>
2018-11-06 11:02:12 -0300

    Cambios en el bat

--- TestSFML/TestSFML/copyfiles.bat
+++ TestSFML/TestSFML/copyfiles.bat
@@ -7,11 +7,18 @@ set targetDir=%1
 
 set solutionDir=%2
 
-echo "%targetDir%"
-echo "%solutionDir%"
+set configuration=%3
 
 MKDIR "%targetDir%res"
 XCOPY "%solutionDir%res" "%targetDir%res" /E /Y /S
-XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-graphics-2.dll" "%targetDir%" /E /Y /S
-XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-system-2.dll" "%targetDir%" /E /Y /S
-XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-window-2.dll" "%targetDir%" /E /Y /S
+
+if %configuration% == Release (
+	XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-graphics-2.dll" "%targetDir%" /E /Y /S
+	XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-system-2.dll" "%targetDir%" /E /Y /S
+	XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-window-2.dll" "%targetDir%" /E /Y /S
+)
+if %configuration% == Debug (
+	XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-graphics-d-2.dll" "%targetDir%" /E /Y /S
+	XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-system-d-2.dll" "%targetDir%" /E /Y /S
+	XCOPY "%solutionDir%libs\SFML-2.5.1\bin\sfml-window-d-2.dll" "%targetDir%" /E /Y /S
+)
